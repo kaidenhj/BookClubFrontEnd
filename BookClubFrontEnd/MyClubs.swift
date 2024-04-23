8256f673b39c7deb839e20094ab39f870ca98672
kaidenhj <kaidenhj@icloud.com>
2024-04-22 20:43:53 -0400

    ADDED BUTTONS!!!!!!

---
+++ BookClubFrontEnd/MyClubs.swift
@@ -0,0 +1,18 @@
+//
+//  MyClubs.swift
+//  BookClubFrontEnd
+//
+//  Created by Kaiden Jones on 4/22/24.
+//
+
+import SwiftUI
+
+struct MyClubs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyClubs()
+}
---
+++ BookClubFrontEnd/UserProfile.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfile.swift
+//  BookClubFrontEnd
+//
+//  Created by Kaiden Jones on 4/22/24.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfile()
+}
