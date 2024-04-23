5f9d08037ec04d1164b0c76c3835c618caeef44a
kaidenhj <kaidenhj@icloud.com>
2024-04-23 10:29:17 -0400

    setting up club universal image

---
+++ BookClubFrontEnd/view/clubs/ClubRow.swift
@@ -0,0 +1,18 @@
+//
+//  ClubRow.swift
+//  BookClubFrontEnd
+//
+//  Created by Kaiden Jones on 4/23/24.
+//
+
+import SwiftUI
+
+struct ClubRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClubRow()
+}
