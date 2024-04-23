ecbdc78723e7da7896ac6bcec6b06834abc6395b
cprochilo <cprochilo@ithaca.edu>
2024-04-22 21:00:35 -0400

    Started making user profile.

--- BookClubFrontEnd/UserProfile.swift
+++ BookClubFrontEnd/UserProfile.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 struct UserProfile: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+                HStack{
+                    Text("First Name: ")
+                }
+            HStack{
+                Text("Last Name: ")
+            }
+            }.padding()
+        }
     }
-}
 
 #Preview {
     UserProfile()
