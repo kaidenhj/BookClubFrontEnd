f4879acfcde64e398aa73596a76bee534a0f8916
kaidenhj <kaidenhj@icloud.com>
2024-04-27 19:10:12 -0400

    commit before puling cole's code

--- BookClubFrontEnd/model/User.swift
+++ BookClubFrontEnd/model/User.swift
@@ -13,6 +13,8 @@ struct User: Codable {
     var id : String
     var name : String
     var password : String
+    
+    var books : [Book]
     struct Book: Codable {
         let id: String
         let volumeInfo: VolumeInfo
@@ -26,7 +28,8 @@ struct User: Codable {
             }
         }
     }
-    var books : [Book]
+    
+    var clubs : [Club]
     struct Club : Codable{
         var id : String
         var name : String
@@ -36,13 +39,26 @@ struct User: Codable {
         var books : [Book]
         var meetingLocation : String
     }
-    var clubs : [Club]
     var ownedClubs : [Club]
+    
     var reviews : [Review]
     struct Review: Codable {
-        var id : Int
-        var user : User
+        var id : String
+        var user : String
         var book : Book
+        struct Book: Codable {
+            let id: String
+            let volumeInfo: VolumeInfo
+            struct VolumeInfo: Codable {
+                let title: String
+                let authors: [String]
+                let description: String
+                let imageLinks: ImageLinks
+                struct ImageLinks: Codable {
+                    let smallThumbnail: String
+                }
+            }
+        }
         var date : String
         var content : String
     }
